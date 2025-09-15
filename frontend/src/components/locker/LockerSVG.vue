bb413e25227f8d79e6d5c07a4900af55f354f850
Locker4 Developer <dev@locker4.com>
2025-09-15 16:04:37 +0900

    fix: 이용 기간 날짜 1px 위로 이동
    - 하단 날짜 영역의 이용 기간 텍스트 위치 조정
    - Y 좌표에서 -1 하여 1px 위로 이동

--- frontend/src/components/locker/LockerSVG.vue
+++ frontend/src/components/locker/LockerSVG.vue
@@ -332,7 +332,7 @@
       <text
         v-if="props.lockerStatus.startDate && props.lockerStatus.endDate"
         :x="logicalDimensions.width / 2"
-        :y="logicalDimensions.height - 3 * LOCKER_VISUAL_SCALE"
+        :y="logicalDimensions.height - 3 * LOCKER_VISUAL_SCALE - 1"
         text-anchor="middle"
         dominant-baseline="middle"
         :font-size="fontSize * 0.7"
