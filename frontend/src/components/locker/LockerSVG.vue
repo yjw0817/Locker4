8f2d9251fc850ff36eb6803d3ac820c2bc1d4fc7
Locker4 Developer <dev@locker4.com>
2025-09-18 13:15:21 +0900

    Reduce LockerManagement date band height by 0.5px

--- frontend/src/components/locker/LockerSVG.vue
+++ frontend/src/components/locker/LockerSVG.vue
@@ -320,8 +320,8 @@
       <path
         v-if="props.lockerStatus.startDate && props.lockerStatus.endDate"
         :d="`
-          M 0 ${logicalDimensions.height - 7 * LOCKER_VISUAL_SCALE}
-          L ${logicalDimensions.width} ${logicalDimensions.height - 7 * LOCKER_VISUAL_SCALE}
+          M 0 ${logicalDimensions.height - 7 * LOCKER_VISUAL_SCALE - 0.5}
+          L ${logicalDimensions.width} ${logicalDimensions.height - 7 * LOCKER_VISUAL_SCALE - 0.5}
           L ${logicalDimensions.width} ${logicalDimensions.height - cornerRadius}
           Q ${logicalDimensions.width} ${logicalDimensions.height} ${logicalDimensions.width - cornerRadius} ${logicalDimensions.height}
           L ${cornerRadius} ${logicalDimensions.height}
@@ -350,7 +350,7 @@
       <text
         v-if="props.lockerStatus.startDate && props.lockerStatus.endDate"
         :x="logicalDimensions.width / 2"
-        :y="logicalDimensions.height - 3 * LOCKER_VISUAL_SCALE"
+        :y="logicalDimensions.height - 3 * LOCKER_VISUAL_SCALE - 0.5"
         text-anchor="middle"
         dominant-baseline="middle"
         :font-size="fontSize * 0.7"
